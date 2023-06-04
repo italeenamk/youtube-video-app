ef428ddb50f5c15b831df0108d2e795f3083526d
italeenamk <italeenamk@gmail.com>
2023-06-04 20:56:21 +0500

    done

--- api/models/Comment.js
+++ api/models/Comment.js
@@ -5,37 +5,13 @@ const CommentSchema = new mongoose.Schema({
             type: String,
             required: true,
         },
-        title:{
+        videoId:{
             type: String,
             required: true,
         },
         description:{
             type: String,
             required: true,
-        },
-        imgUrl:{
-            type: String,
-            required: true,
-        },
-        videoUrl:{
-            type: String,
-            required: true,
-        },
-        views:{
-            type: Number,
-            default: 0,
-        },
-        tags:{
-            type: [String],
-            default: [],
-        },
-        likes:{
-            type: [String],
-            default: [],
-        },
-        dislike:{
-            type: [String],
-            default: [],
         },
     },
     { timestamps: true }
