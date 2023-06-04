72cc3af39df029a5f721c269543607547f0ae175
italeenamk <italeenamk@gmail.com>
2023-06-04 20:35:32 +0500

    create userschema

--- api/models/User.js
+++ api/models/User.js
@@ -1,1 +1,28 @@
 const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    img:{
+        type:String,
+    },
+    subscribers:{
+        type:Number,
+        default:0
+    },
+    subscriberUsers:{
+        type:[String]
+    }
+});
